176b53ef756a9351f5aec86beec63e3640ffb399
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-02-22 21:20:59 -0700

    Adding MemoryGameDAO.clear() to JUnit tests

--- server/src/test/java/serviceTests/GameServiceTest.java
+++ server/src/test/java/serviceTests/GameServiceTest.java
@@ -19,6 +19,7 @@ class GameServiceTest {
     void setUp() throws DataAccessException {
         MemoryUserDAO.clear();
         MemoryAuthDAO.clear();
+        MemoryGameDAO.clear();
     }
 
     @Test
