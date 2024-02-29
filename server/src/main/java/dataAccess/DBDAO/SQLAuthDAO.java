10e5fe9697e5bcdcb3a4a1e1a36f2dd2e4c1fb8e
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-02-29 09:07:03 -0700

    Stubbed out methods for SQLAuthDAO

--- server/src/main/java/dataAccess/DBDAO/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/DBDAO/SQLAuthDAO.java
@@ -1,5 +1,24 @@
 package dataAccess.DBDAO;
 
-public class SQLAuthDAO implements dataAccess.AuthDAO {
+import dataAccess.DataAccessException;
+import model.AuthData;
 
+import java.util.HashSet;
+
+public class SQLAuthDAO implements dataAccess.AuthDAO {
+    public static AuthData createAuth(String username) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static void deleteAuth(String authIdentifier) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static AuthData verifyAuth(String authToken) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static void clear() throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static HashSet<AuthData> listAuths() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
