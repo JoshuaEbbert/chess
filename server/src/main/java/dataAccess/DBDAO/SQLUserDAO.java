84044ab4f0d96c1a5a86e60a6bbb4c3c5512be8d
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-02-29 09:07:39 -0700

    Stubbed out methods for SQLUserDAO

--- server/src/main/java/dataAccess/DBDAO/SQLUserDAO.java
+++ server/src/main/java/dataAccess/DBDAO/SQLUserDAO.java
@@ -1,4 +1,16 @@
 package dataAccess.DBDAO;
 
+import dataAccess.DataAccessException;
+import model.UserData;
+
 public class SQLUserDAO implements dataAccess.UserDAO {
+    public static void createUser(String username, String password, String email) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static UserData getUser(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public static void clear() throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
